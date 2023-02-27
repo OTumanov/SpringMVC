d95d5215c2e7018169038a83cb25025ecccc136b
olegtumanov <otumanov@gmail.com>
2023-02-27 09:11:29 +0300

    lesson 17.1

--- src/main/java/ru/tumanov/springMVC/controllers/FirstController.java
+++ src/main/java/ru/tumanov/springMVC/controllers/FirstController.java
@@ -2,8 +2,10 @@ package ru.tumanov.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/new")
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage() {
