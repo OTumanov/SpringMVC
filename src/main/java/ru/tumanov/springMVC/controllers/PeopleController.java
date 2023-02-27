1c1a7fef56fe0c4303dd9116a38066a5688ec37e
it-2 <otumanov@gmail.com>
2023-02-27 21:16:51 +0300

    Spring Framework. Урок 22: Аннотация @ModelAttribute. HTML Формы (Thymeleaf).

--- src/main/java/ru/tumanov/springMVC/controllers/PeopleController.java
+++ src/main/java/ru/tumanov/springMVC/controllers/PeopleController.java
@@ -2,10 +2,9 @@ package ru.tumanov.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.tumanov.springMVC.dao.PersonDAO;
+import ru.tumanov.springMVC.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -26,5 +25,17 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPeople(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
