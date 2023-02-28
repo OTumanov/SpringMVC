90d49e3e718ebc5dff224580c9f02d19e6669577
olegtumanov <otumanov@gmail.com>
2023-02-28 14:39:25 +0300

    Spring Framework. Урок 24: Валидация форм. Аннотация @Valid.

--- src/main/java/ru/tumanov/springMVC/controllers/PeopleController.java
+++ src/main/java/ru/tumanov/springMVC/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.tumanov.springMVC.controllers;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.tumanov.springMVC.dao.PersonDAO;
 import ru.tumanov.springMVC.models.Person;
@@ -35,7 +37,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -47,7 +53,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         @PathVariable("id") int id,
+                         BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
